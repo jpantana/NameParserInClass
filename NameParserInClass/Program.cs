8adb1c6c07fdb99c046b03f46ffad3bd70e3498c
jpantana <josh.pantana@gmail.com>
2019-09-14 09:55:53 -0500

    Add project files.

---
+++ NameParserInClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NameParserInClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
